de0f090743fdd62a48ce971d8314727962d32293
Duongph20350 <108260247+Duongph20350@users.noreply.github.com>
2024-05-20 22:33:42 +0700

    Update sql layout
    Update sql layout

--- DATN_SUM24_SD01/src/main/java/com/example/datnsum24sd01/controller/ChiTietSanPhamCustomerController.java
+++ DATN_SUM24_SD01/src/main/java/com/example/datnsum24sd01/controller/ChiTietSanPhamCustomerController.java
@@ -21,46 +21,6 @@ public class ChiTietSanPhamCustomerController {
        return "customer-template/index";
     }
 
-//    @GetMapping("/chi-tiet-san-pham/{id}")
-//    public String detailCustomerSanPham(@PathVariable("id") Long id, Model model) {
-//        Long idKh=principalKhachHang.getCurrentUserId();
-//        Boolean checkSecurity=false;
-//        if (idKh == null) {
-//            model.addAttribute("checkSecurity",checkSecurity);
-//            ChiTietSanPham chiTietSanPham = chiTietSanPhamService.getById(id);
-//            model.addAttribute("spDetail", chiTietSanPham);
-//            List<AnhCustomerCustom> listAnhdetail = chiTietSanPhamService.listAnhDetail(id);
-//            model.addAttribute("listAnhDetail", listAnhdetail.stream().toList());
-//            List<ChiTietSanPhamCustomerCustom> listRand = chiTietSanPhamService.list4Random();
-//            model.addAttribute("listRandom", listRand.stream().toList());
-//            List<ChiTietSanPhamCustomerCustom> listRand2 = chiTietSanPhamService.list4Random();
-//            model.addAttribute("listRandom2", listRand2.stream().toList());
-//            model.addAttribute("soLuongTon", chiTietSanPham.getSoLuongTon());
-//        }else {
-//            checkSecurity= true;
-//            model.addAttribute("checkSecurity",checkSecurity);
-//            ChiTietSanPham chiTietSanPham = chiTietSanPhamService.getById(id);
-//            model.addAttribute("spDetail", chiTietSanPham);
-//            List<AnhCustomerCustom> listAnhdetail = chiTietSanPhamService.listAnhDetail(id);
-//            model.addAttribute("listAnhDetail", listAnhdetail.stream().toList());
-//            List<ChiTietSanPhamCustomerCustom> listRand = chiTietSanPhamService.list4Random();
-//            model.addAttribute("listRandom", listRand.stream().toList());
-//            List<ChiTietSanPhamCustomerCustom> listRand2 = chiTietSanPhamService.list4Random();
-//            model.addAttribute("listRandom2", listRand2.stream().toList());
-//            model.addAttribute("soLuongTon", chiTietSanPham.getSoLuongTon());
-//            List<GioHangChiTiet> cartItems = gioHangChiTietService.getAll(idKh);
-//
-//            // Tìm mục trong giỏ hàng dựa trên ID sản phẩm
-//            GioHangChiTiet gioHangChiTiet = cartItems.stream()
-//                    .filter(item -> item.getChiTietSanPham().getId().equals(id))
-//                    .findFirst()
-//                    .orElse(null);
-//
-//            int soLuongTrongGioHang = (gioHangChiTiet != null) ? gioHangChiTiet.getSoLuong() : 0;
-//            model.addAttribute("soLuongTrongGioHang", soLuongTrongGioHang);
-//        }
-//
-//        return "customer-template/detail";
-//    }
+
 
 }
