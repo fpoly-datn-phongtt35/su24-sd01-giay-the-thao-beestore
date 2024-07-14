634f6a9a4dc412144699e2f0af874b9da2bbfb07
Duongph20350 <108260247+Duongph20350@users.noreply.github.com>
2024-07-14 14:06:07 +0700

    update sp
    update sp

--- DATN_SUM24_SD01/src/main/java/com/example/datnsum24sd01/responsitory/SanPhamResponsitory.java
+++
@@ -1,7 +0,0 @@
-package com.example.datnsum24sd01.responsitory;
-
-import com.example.datnsum24sd01.entity.SanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SanPhamResponsitory  {
-}
