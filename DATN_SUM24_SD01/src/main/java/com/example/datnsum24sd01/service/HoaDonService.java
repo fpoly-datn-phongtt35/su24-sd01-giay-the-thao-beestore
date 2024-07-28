9073f9c2af2b2cf6dac0ee60d26a47b862f19066
Duongph20350 <108260247+Duongph20350@users.noreply.github.com>
2024-07-29 01:25:32 +0700

    update giohang
    update giohang

--- DATN_SUM24_SD01/src/main/java/com/example/datnsum24sd01/service/HoaDonService.java
+++ DATN_SUM24_SD01/src/main/java/com/example/datnsum24sd01/service/HoaDonService.java
@@ -11,7 +11,33 @@ import java.util.List;
 public interface HoaDonService {
     List<HoaDon> getAll();
 
+    List<HoaDon> getAllByKhachHang(Long id);
 
-    BigDecimal phieuGiamGia(Long idHd);
+
+    List<HoaDon> getByTrangThai(TrangThaiDonHang trangThai);
+
+    List<HoaDon> getByTrangThaiAndKhachHang(TrangThaiDonHang trangThai,Long id);
+
+    HoaDon findById(Long id);
+
+    HoaDon save(HoaDon hoaDon);
+
+    List<HoaDon> findHoaDonsByNgayTao(LocalDate start, LocalDate end);
+
+    boolean validate(HoaDon hoaDon, TrangThaiDonHang trangThai, String newGhiChu);
+
+
+    HoaDon createHdHoanTra(HoaDon hoaDon,Long idHd);
+
+//    boolean removeGioHangChiTietHoanTra(GioHangChiTiet gioHangChiTiet,HoaDon hoaDon);
+//
+//
+//    ChiTietSanPham refund(GioHangChiTiet gioHangChiTiet);
+
+    BigDecimal maGiamGia(Long idHd);
+
+    public HoaDon findByMa(String maHd);
+
+
 
 }
